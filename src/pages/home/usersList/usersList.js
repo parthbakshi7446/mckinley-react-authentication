f64f1edc4dbba8b1322900fd970a172e5cd798e3
parthbakshi7446 <parthbakshi7446@gmail.com>
2020-07-20 15:51:20 +0530

    add userlist

--- src/pages/home/usersList/usersList.js
+++ src/pages/home/usersList/usersList.js
@@ -1,22 +1,42 @@
 import React, { Component } from "react";
+import axios from "axios";
+import Spinner from "../../../components/UI/Spinner/Spinner";
+// import UserLoading from "../../../components/userLoading";
+// import * as actions from "./actions";
+// import { bindActionCreators } from "redux";
+// import { connect } from "react-redux";
 import { Row, Avatar, UserInfo, Text } from "./styles";
+// import PropTypes from "prop-types";
 
 export class UsersList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { users: null, loading: false };
+  }
   componentDidMount() {
-    // only call Api request if stored data is not completed
-    if (this.props.total_pages >= this.props.next_page) {
-      this.request();
+    if (!this.state.users) {
+      axios
+        .get("https://reqres.in/api/users")
+        .then((res) => {
+          console.log(res)
+          this.setState({ users: res.data.data });
+        })
+        .catch((error) => {
+          this.setState({ error: true });
+        });
     }
   }
-  handleLoadMore = e => {
-    e.preventDefault();
-    //add new data
-    this.request();
-  };
-  request = () => {
-    this.props.get("&page=" + this.props.next_page);
-  };
-  renderFarm = (items =[]) => {
+  // handleLoadMore = e => {
+  //   e.preventDefault();
+  //   //add new data
+  //   this.request();
+  // };
+  // request = () => {
+  //   this.props.get("&page=" + this.props.next_page);
+  // };
+  renderFarm = (items = []) => {
+    // console.log(items)
+    if(items)
     return items.map((item, index) => {
       return (
         <Row key={index} id={item.id}>
@@ -24,19 +44,29 @@ export class UsersList extends Component {
             <img src={item.avatar} alt={item.first_name} />
           </Avatar>
           <UserInfo>
-            <Text color={"firstname"} className='firstname'>{item.first_name}</Text>
-            <Text color={"lastname"}  className='lastname'>{item.last_name}</Text>
+            <Text color={"firstname"} className="firstname">
+              {item.first_name}
+            </Text>
+            <Text color={"lastname"} className="lastname">
+              {item.last_name}
+            </Text>
           </UserInfo>
         </Row>
       );
     });
   };
   render() {
-    const { data, loading, next_page, total_pages, failure } = this.props;
+    // const {  loading, next_page, total_pages, failure } = this.props;
+    let users = <Spinner />
+    if (this.state.users)
+      users=this.renderFarm(this.state.users)
+
     return (
       <>
-        {this.renderFarm(data)}
-        <div style={{ textAlign: "center" }}>
+      
+        {users}
+        {/* <UserLoading isLoading={loading} /> */}
+        {/* <div style={{ textAlign: "center" }}>
           {total_pages >= next_page && !loading && (
             <button className="btn load_more" onClick={this.handleLoadMore}>
               Load more...
@@ -47,10 +77,45 @@ export class UsersList extends Component {
               Try again
             </button>
           )}
-        </div>
+        </div> */}
       </>
     );
   }
 }
 
+// const mapStateToProps = state => ({
+//   loading: state.usersList.loading,
+//   failure: state.usersList.failure,
+//   data: state.usersList.data,
+//   next_page: state.usersList.next_page,
+//   total_pages: state.usersList.total_pages
+// });
+// const mapDispatchToProps = dispatch => {
+//   const { get, deleteItem } = actions;
+//   return bindActionCreators(
+//     { get, deleteItem },
+//     dispatch
+//   );
+// };
+// UsersList.propTypes = {
+//     loading : PropTypes.bool,
+//     failure: PropTypes.bool,
+//     data: PropTypes.array,
+//     next_page: PropTypes.number,
+//     total_pages: PropTypes.number,
+//     get: PropTypes.func,
+//     deleteItem: PropTypes.func,
+// };
 export default UsersList;
+
+// class UsersList extends Component {
+
+//   constructor(props){
+//     super(props)
+//     this.state={
+//       users:null,
+//       loading:false,error:false
+//     }
+//   }
+
+// }
